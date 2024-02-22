bca147bc3af495442adabba54e3f2847385a3633
zzangyou <534994484@qq.com>
2024-02-22 15:11:43 +0800

    feat: axios 封装

--- client/src/api/request.js
+++ client/src/api/request.js
@@ -1,17 +1,36 @@
 // 该文件用于【对axios进行二次封装】
 // 引入axios
 import axios ,{ AxiosInstance, AxiosRequestConfig }from 'axios';
-import config from '@/config'
-import { ElMessage } from 'element-plus'
+import { Message } from 'ant-design-vue'
 import { router } from '@/router/index'
-import { LS_KEYS } from '@/utils/const';
+
 const NETWORK_ERROR = '网络请求异常，请稍后重试'
+let loadingInstance=null;
+let requestNum = 0;
+
+const addLoading = () => {
+  // 增加loading 如果pending请求数量等于1，弹出loading, 防止重复弹出
+  requestNum++;
+  if (requestNum == 1) {
+    loadingInstance = message.loading({
+      text: "正在努力加载中....",
+  
+    });
+  }
+};
+
+const cancelLoading = () => {
+  // 取消loading 如果pending请求数量等于0，关闭loading
+  requestNum--;
+  if (requestNum === 0) loadingInstance?.destroy();
+};
+
+
 // 添加 自定义配置的【新的axios】
-const service = axios.create({//利用axios.create,创建一个新的axios实例并做一些配置
-  // 根据环境 设置不同的 baseURL(基础路径，发请求的时候，路径当中会出现所配置的基础路径，不用自己手写)
-  // 配置了前端反向代理,直接'/public'代理了本地数据的请求接口,后边跟的是线上的接口地址
-  baseURL: config.baseApi,
+const service = axios.create({//利用axios.create,
+  // baseURL: config.baseApi,
   // 超时
+  withCredentials: true,  //跨域携带cookie
   timeout: 10000,
 })
 // 请求拦截器 - 所有的请求开始之前先到此处
@@ -20,7 +39,9 @@ service.interceptors.request.use((config) => {
   // 可以设置 加载的动画效果 的展示
   // 在必要的路由设置一些额外的参数 ---- token信息携带放在此处
   // 将token放到请求头中发送给服务器
-  const token = localStorage.getItem(LS_KEYS.JWT)
+  const { loading = true } = config;
+  const token = localStorage.getItem('token')
+  if (loading) addLoading();
   if (token) {
     // 请求头携带token
     config.headers['Authorization'] = token
@@ -28,7 +49,7 @@ service.interceptors.request.use((config) => {
   console.log('正在加载....')
   return config
 }, (error) => {
-  ElMessage.error(error)
+  Message.error(error||NETWORK_ERROR)
   return Promise.reject(error)
 })
 // http response 拦截器
@@ -37,22 +58,27 @@ service.interceptors.request.use((config) => {
 axios.interceptors.response.use(
   response => {
     // 成功的回调函数：服务器响应数据回来后，响应拦截器可检测到，就可以做一些事情
+    const { loading = true } = response.config;
+      if (loading) cancelLoading();
     return response;  //请求成功的时候返回一个数据
   },
   error => {
     try {
+      const { loading = true } = error.config;
+      if (loading) cancelLoading();
       if (error.response) {
         switch (error.response.status) {
           case 401://token过期，清除token并跳登录页面
             localStorage.clear();
             var baurl = window.location.href;
             router.replace({
-              path: 'login',
+              path: '/user',
               query: { backUrl: baurl }
             });
             return;
         }
       }
+      Message.error(error?.response?.data?.message || NETWORK_ERROR);
       return Promise.reject(error.response.data)
 
     }
